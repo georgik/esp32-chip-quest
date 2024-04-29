2d08a02a6883674b0c901a81b3b0d938e3a50485
Juraj Michálek <juraj.michalek@espressif.com>
2024-04-29 15:09:30 +0200

    display socs

--- js/maker-choice.js
+++ js/maker-choice.js
@@ -13,25 +13,121 @@ const chipProperties = {
         'availability': {
             'start': '2016',
             'end': '2028'
-        }
+        },
+        'devkits': [
+            {
+                'name': 'ESP32-DevKitC',
+                'image': 'images/esp32-devkitc.jpg'
+            },
+            {
+                'name': 'ESP32-DevKitM',
+                'image': 'images/esp32-devkitm.jpg'
+            }
+        ],
+        'modules': [
+            {
+                'name': 'ESP32-WROOM-32',
+                'image': 'images/esp32-wroom-32.jpg'
+            },
+            {
+                'name': 'ESP32-WROVER',
+                'image': 'images/esp32-wrover.jpg'
+            }
+        ],
+        'socs': [
+            {
+                'name': 'ESP32',
+                'image': 'images/esp32-socs.png'
+            }
+        ]
     },
     'ESP32-S3': {
         'availability': {
             'start': '2022',
             'end': '2034'
-        }
+        },
+        'devkits': [
+            {
+                'name': 'ESP32-S3-BOX-3',
+                'image': 'https://www.espressif.com/sites/default/files/dev-board/ESP32-S3-BOX-3_S.png'
+            },
+            {
+                'name': 'M5Stack CoreS3',
+                'image': 'https://static-cdn.m5stack.com/resource/docs/products/core/CoreS3/img-9618aed2-e595-4bb4-ac53-e0b9772aefd6.webp'
+            },
+            {
+                'name': 'ESP32-S3-DevKitM-1',
+                'image': 'https://www.espressif.com/sites/default/files/dev-board/ESP32-S3-DevKitM-1_S.png'
+            },
+            {
+                'name': 'ESP32-S3-DevKitC-1',
+                'image': 'https://www.espressif.com/sites/default/files/dev-board/ESP32-S3-DevKitC-1%20%E5%B0%8F.png'
+            }
+        ],
+        'modules': [
+            {
+                'name': 'ESP32-S3-WROOM-1',
+                'image': 'https://www.espressif.com/sites/default/files/modules/ESP32-S3-WROOM-1%20S_0.png'
+            },
+            {
+                'name': 'ESP32-S3-MINI-1',
+                'image': 'https://www.espressif.com/sites/default/files/modules/ESP32-S3-MINI-1%20S_0.png'
+            }
+        ],
+        'socs': [
+            {
+                'name': 'ESP32-S3',
+                'image': 'https://www.espressif.com/sites/default/files/chips/ESP32-S3_S_0.png'
+            },
+            {
+                'name': 'ESP32-S3-PICO-1',
+                'image': 'https://www.espressif.com/sites/default/files/chips/ESP32-S3-PICO-1_S_0.png'
+            }
+        ]
     },
     'ESP32-C6': {
         'availability': {
             'start': '2023',
             'end': '2035'
-        }
+        },
+        'devkits': [
+            {
+                'name': 'ESP32-C6-DevKitC-1',
+                'image': 'images/esp32-c6-devkitc-1.jpg'
+            },
+            {
+                'name': 'ESP32-C6-DevKitM-1',
+                'image': 'images/esp32-c6-devkitm-1.jpg'
+            }
+        ],
+        'modules': [
+            {
+                'name': 'ESP32-C6-WROOM-1',
+                'image': 'images/esp32-c6-wroom-1.jpg'
+            }
+        ],
+        'socs': [
+            {
+                'name': 'ESP32-C6',
+                'image': 'images/esp32-c6-socs.png'
+            }
+        ]
     },
     'ESP32-P4': {
         'availability': {
             'start': '2024',
             'end': '2036'
-        }
+        },
+        'devkits': [
+            {
+                'name': 'ESP32-P4-DevKitC-1',
+                'image': 'images/esp32-p4-devkitc-1.jpg'
+            },
+            {
+                'name': 'ESP32-P4-DevKitM-1',
+                'image': 'images/esp32-p4-devkitm-1.jpg'
+            }
+        ],
     }
 };
 
@@ -91,11 +187,48 @@ document.getElementById('current-chip-box').addEventListener('click', function()
     toggleFormFactors();
 });
 
+
+function renderDevKits() {
+    const devkits = chipProperties[chips[currentChipIndex]].devkits;
+    const devkitList = document.getElementById('devkit-list');
+    devkitList.innerHTML = devkits.map(devkit => `
+        <li>
+            <img class="devkit-thumbnail" src="${devkit.image}" alt="${devkit.name}">
+            <span>${devkit.name}</span>
+        </li>
+    `).join('');
+}
+
+function renderModules() {
+    const modules = chipProperties[chips[currentChipIndex]].modules;
+    const moduleList = document.getElementById('module-list');
+    moduleList.innerHTML = modules.map(module => `
+        <li>
+            <img src="${module.image}" alt="${module.name}">
+            <span>${module.name}</span>
+        </li>
+    `).join('');
+}
+
+function renderChips() {
+    const socs = chipProperties[chips[currentChipIndex]].socs;
+    const socList = document.getElementById('chip-list');
+    socList.innerHTML = socs.map(soc => `
+        <li>
+            <img src="${soc.image}" alt="${soc.name}">
+            <span>${soc.name}</span>
+        </li>
+    `).join('');
+}
+
 function toggleFormFactors() {
     var formFactors = document.querySelector('.form-factors');
     if (formFactors.style.display === "none") {
         formFactors.style.display = "flex";
         formFactorsVisible = true;
+        renderDevKits();
+        renderModules();
+        renderChips();
     } else {
         formFactors.style.display = "none";
         formFactorsVisible = false;
@@ -122,9 +255,3 @@ function showFormFactorDetails(formFactor) {
     // Implement actual display logic or update page content here
 }
 
-
-window.addEventListener('resize', function() {
-    if (formFactorsVisible) {
-        drawConnectors(); // Only redraw if form factors are visible
-    }
-});
